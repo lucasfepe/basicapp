0ae8b2b7d0eb734fd34de485fe6e9335275f3b93
lucas.pereira@fdmgroup.com <lucasferrari4@gmail.com>
2020-11-10 10:30:31 -0700

    2

--- src/main/java/com/fdmgroup/notetrainer/controller/SoundController.java
+++ src/main/java/com/fdmgroup/notetrainer/controller/SoundController.java
@@ -1,6 +1,6 @@
 package com.fdmgroup.notetrainer.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;	
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
